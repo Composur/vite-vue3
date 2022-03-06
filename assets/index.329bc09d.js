0f461caa39a836bf9579825e06f0b780017578d8
Composur <Composur@users.noreply.github.com>
2022-03-06 15:46:40 +0000

    feat: micro test a32c4041e21b3b87295f37f8512dbdc1746a3c43

--- assets/index.329bc09d.js
+++
@@ -1,1 +0,0 @@
-import{l as s,G as o}from"./vendor.55c1fbf9.js";const c={created(){const e=s(),{params:r,query:t}=o(),{path:a}=r;e.replace({path:`/${a}`,query:t})},render(e){return e()}};export{c as default};
---
+++ assets/index.5725b36e.js
@@ -0,0 +1,1 @@
+import{G as s,l as o}from"./vendor.305bd43c.js";const c={created(){const e=s(),{params:r,query:t}=o(),{path:a}=r;e.replace({path:`/${a}`,query:t})},render(e){return e()}};export{c as default};
