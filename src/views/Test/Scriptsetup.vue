4216eac2c231b8633c6d7a27dab968fbd49853bc
Edison <xutong1107@gmail.com>
2022-03-15 23:29:39 +0800

    test: script-setup

---
+++ src/views/Test/Scriptsetup.vue
@@ -0,0 +1,30 @@
+<template>
+  <a-row>
+    <a-col :span="4">
+      <p>{{ name }}</p>
+    </a-col>
+  </a-row>
+  <ScriptsetupSon
+    ref="son"
+    :name="name"
+    msg="父组件传递的msg"
+    data-msg="父组件传递的data-msg"
+    @change-name="changeName"
+  />
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import ScriptsetupSon from './ScriptsetupSon.vue'
+
+const name = ref('父组件传递的name')
+const son = ref(null)
+onMounted(() => {
+  console.log(son.value.name)
+})
+const changeName = (n) => {
+  name.value = n
+}
+</script>
+
+<style scoped></style>
---
+++ src/views/Test/ScriptsetupSon.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="page-title">{{ pageTitle }}</div>
+  <a-row>
+    <a-col :span="5">{{ attrs.msg }}</a-col>
+    <a-col :span="5">{{ props.name }}</a-col>
+    <a-col :span="4"><a-button @click="clickHandle">改变父的name</a-button></a-col>
+  </a-row>
+  <a-row> </a-row>
+</template>
+
+<script setup lang="ts">
+// 导入 useAttrs 组件
+import { useAttrs } from 'vue'
+
+const props = defineProps({
+  name: {
+    type: String,
+    default: 'name'
+  }
+})
+const pageTitle = 'script-setup'
+
+// 获取 emit
+const emit = defineEmits(['change-name'])
+
+const clickHandle = () => {
+  // 调用 emit
+  emit('change-name', 'Tom')
+}
+
+// 获取 attrs
+const attrs = useAttrs()
+
+// attrs是个对象，和 props 一样，需要通过 key 来得到对应的单个 attr
+console.log(attrs)
+
+defineExpose({
+  name: props.name
+})
+</script>
+
+<style scoped></style>
