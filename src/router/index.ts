95664f25d934e5b34d50c6b8f1ddd13a53a815cf
Edison <xutong1107@gmail.com>
2022-03-17 12:19:57 +0800

    fix: deploy dir

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,18 @@
 /**
  * 路由的创建、拦截等逻辑功能
  */
-import { createRouter, createWebHistory, Router } from 'vue-router'
+import {
+  createRouter,
+  createWebHistory,
+  createWebHashHistory,
+  Router
+} from 'vue-router'
 import routes from './routes'
 
+const { GITHUB } = import.meta.env
+const history = GITHUB ? createWebHashHistory() : createWebHistory()
 const router: Router = createRouter({
-  history: createWebHistory(),
+  history,
   routes
 })
 
