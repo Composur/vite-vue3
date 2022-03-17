a22df20a83cc430a11f951f69b29adcdba11a88d
composure <xutong1107@gmail.com>
2022-03-17 21:40:29 +0800

    fix: test

--- tests/Header.spec.ts
+++ tests/Header.spec.ts
@@ -1,10 +1,10 @@
-// import { mount } from '@vue/test-utils'
-// // @ts-ignore
-// import Header from '../src/components/Header.vue'
+import { mount } from '@vue/test-utils'
+// @ts-ignore
+import Header from '../src/components/Header.vue'
 
-// describe('Header.vue', () => {
-//   it('renders', () => {
-//     const wrapper = mount(Header)
-//     expect(wrapper.html()).toContain('Vite2.x + Vue3.x + TypeScript Starter')
-//   })
-// })
+describe('Header.vue', () => {
+  it('renders', () => {
+    const wrapper = mount(Header)
+    expect(wrapper.html()).toContain('test')
+  })
+})
